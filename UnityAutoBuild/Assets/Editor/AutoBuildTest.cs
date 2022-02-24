cf2304dadbb1f0e8ce4487c992179c1fe687ce72
BanMing <ban-ming@foxmail.com>
2022-02-24 15:48:00 +0800

    add new test

--- UnityAutoBuild/Assets/Editor/AutoBuildTest.cs
+++ UnityAutoBuild/Assets/Editor/AutoBuildTest.cs
@@ -1,5 +1,7 @@
 ﻿using UnityEngine;
 using UnityEditor;
+using UnityEditor.Build.Reporting;
+using System;
 
 public class AutoBuildTest
 {
@@ -12,16 +14,16 @@ public class AutoBuildTest
         PlayerSettings.applicationIdentifier = "com.test.autobuild";
         buildPlayerOptions.locationPathName = "Package/Android Build.apk";
         buildPlayerOptions.target = BuildTarget.Android;
-        buildPlayerOptions.options = BuildOptions.Il2CPP | BuildOptions.CompressWithLz4;
+        buildPlayerOptions.options = BuildOptions.CompressWithLz4;
         var error = BuildPipeline.BuildPlayer(buildPlayerOptions);
-        if (!string.IsNullOrEmpty(error))
-        {
-            UnityEngine.Debug.Log("error:" + error);
-        }
-        else
-        {
-            Debug.Log("build success!");
-        }
+        //if (!string.IsNullOrEmpty(error))
+        //{
+        //    UnityEngine.Debug.Log("error:" + error);
+        //}
+        //else
+        //{
+        //    Debug.Log("build success!");
+        //}
     }
 
     [MenuItem("Build Test/Build iOS Test")]
@@ -32,15 +34,46 @@ public class AutoBuildTest
         buildPlayerOptions.locationPathName = "Package/iOSBuild";
         buildPlayerOptions.target = BuildTarget.iOS;
         buildPlayerOptions.options = BuildOptions.None;
-        var error = BuildPipeline.BuildPlayer(buildPlayerOptions);
-        if (!string.IsNullOrEmpty(error))
-        {
-            UnityEngine.Debug.Log("error:" + error);
-        }
-        else
-        {
-            Debug.Log("build success!");
-        }
+        BuildReport report = BuildPipeline.BuildPlayer(buildPlayerOptions);
+        //report.
+        //if (!string.IsNullOrEmpty(error))
+        //{
+        //    UnityEngine.Debug.Log("error:" + error);
+        //}
+        //else
+        //{
+        //    Debug.Log("build success!");
+        //}
+    }
+
+    [MenuItem("Build Test/Build PC Test")]
+    static void BuildPCTest()
+    {
+        BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
+        buildPlayerOptions.scenes = new[] { "Assets/Scene/dome.unity" };
+        buildPlayerOptions.locationPathName = "Package/2/PcBuild.exe";
+        buildPlayerOptions.target = BuildTarget.StandaloneWindows;
+        buildPlayerOptions.options = BuildOptions.AllowDebugging;
+
+        BuildReport report = BuildPipeline.BuildPlayer(buildPlayerOptions);
+    }
+    [MenuItem("Build Test/Test")]
+    static void BuildTest()
+    {
+        //Debug.Log((BuildOptions.AllowDebugging | BuildOptions.Development));
+        //Debug.Log((int)(BuildOptions.AllowDebugging | BuildOptions.Development));
+        //Debug.Log((BuildOptions.StrictMode));
+        Debug.Log(((int)BuildOptions.StrictMode));
+        //var test = (BuildOptions)(513);
+        //Debug.Log(test);
+        var a = (BuildOptions)Enum.Parse(typeof(BuildOptions), "Development");
+        Debug.Log(a);
+
+        var b = (BuildOptions)Enum.Parse(typeof(BuildOptions), "Development", true);
+        Debug.Log(b);
+
+        var c= (BuildOptions)Enum.Parse(typeof(BuildOptions), "513");
+        Debug.Log(c);
     }
 
     /// <summary>
